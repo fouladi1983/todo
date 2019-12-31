86d0557aab3f518bdad6bd75c5412942df547d93
Mohammadreza Fouladi <m.fouladi@WS-05>
2019-12-31 14:57:29 +0330

    starter point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { ToDoComponent } from './components/to-do/to-do.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ToDoComponent
   ],
   imports: [
     BrowserModule,
